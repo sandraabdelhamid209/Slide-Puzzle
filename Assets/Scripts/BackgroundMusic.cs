f97e8f94020c75666633a7bf947f088f96ccf07e
sandra209 <sandra@flyingsquirrelinteractive.com>
2023-05-27 00:51:30 +0200

    Added Audio

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+  public static BackgroundMusic BgInstance;
+  private void Awake()
+    {
+        if(BgInstance != null && BgInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        BgInstance = this;
+        DontDestroyOnLoad(this);
+    }
+  
+}
